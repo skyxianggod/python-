70e8a85674e63f9c53974b4742ea9106e9b39a9c
gongzhixiang <756477415@qq.com>
2018-03-29 10:30:04 +0800

    糗事百科

---
+++ 元类.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+class MyType(type):
+    def __init__(self,*args,**kwargs):
+
+        print("Mytype __init__",*args,**kwargs)
+
+    def __call__(self, *args, **kwargs):
+        print("Mytype __call__", *args, **kwargs)
+        obj = self.__new__(self)
+        print("obj ",obj,*args, **kwargs)
+        print(self)
+        self.__init__(obj,*args, **kwargs)
+        return obj
+
+    def __new__(cls, *args, **kwargs):
+        print("Mytype __new__",*args,**kwargs)
+        return type.__new__(cls, *args, **kwargs)
+
+print('here...')
+class Foo(object,metaclass=MyType):
+
+
+    def __init__(self,name):
+        self.name = name
+
+        print("Foo __init__")
+
+    def __new__(cls, *args, **kwargs):
+        print("Foo __new__                                                                                                                           ",cls, *args, **kwargs)
+        return object.__new__(cls)
+
+f = Foo("Alex")
+print("f ",f)
+print("fname",f.name)
+
